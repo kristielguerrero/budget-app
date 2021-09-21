a6782b291ac8f733bdd4387a46b928a73debcda1
Kris Tina <kristyguerrero20@gmail.com>
2021-09-21 14:16:36 -0500

    onupgradeneeded request function created

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,4 +1,10 @@
 let db;
 const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function (e) {
+  const db = e.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+// listen for app coming back online
 window.addEventListener("online", checkDatabase);
